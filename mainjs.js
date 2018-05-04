d46504dd89fa2a2d7771d09325816d84a47e109b
johanreji <johanpenatu@gmail.com>
2018-05-05 00:27:39 +0530

    major update

---
+++ mainjs.js
@@ -0,0 +1,37 @@
+let flag=0;
+function hovereffect(){
+let dp=document.getElementsByClassName("ohalf")[0];
+let dppic=document.getElementsByClassName("dp")[0];
+
+let one=document.getElementsByClassName("one")[0];
+let two=document.getElementsByClassName("two")[0];
+let three=document.getElementsByClassName("three")[0];
+if(flag==1)
+{
+dppic.classList.add("bounce");
+dp.classList.remove("dptrans2");
+one.classList.remove('onetrans2');
+two.classList.remove('twotrans2');
+three.classList.remove('threetrans2');
+dp.classList.add("dptrans1");
+one.classList.add('dptrans1');
+two.classList.add('dptrans1');
+three.classList.add('dptrans1');
+flag=0;
+
+	}
+	else{
+		dppic.classList.remove("bounce");
+		dp.classList.remove("dptrans1");
+		one.classList.remove('dptrans1');
+		two.classList.remove('dptrans1');
+		three.classList.remove('dptrans1');
+		dp.classList.add("dptrans2");
+		one.classList.add('onetrans2');
+		two.classList.add('twotrans2');
+		three.classList.add('threetrans2');
+		flag=1;
+	}
+
+
+}
